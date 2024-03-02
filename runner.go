8ee046f2ee072654c99f0b8ba6da246af7c08e02
Kushal <43465488+kushalshit27@users.noreply.github.com>
2024-03-02 17:59:10 +0530

    ✨ Features (#8)
    * :art:  (#6)

--- runner.go
+++ runner.go
@@ -6,17 +6,26 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"math"
 	"net/http"
 	"net/http/httputil"
-	"strings"
 	"time"
 )
 
 type GoQLClientRunner interface {
-	Run(ctx context.Context) interface{}
+	Run(ctx context.Context) (GoQLClientResponse, *GoQLClientError)
+	Debug() GoQLClientRunner
 	RawReq() GoQLClientRunner
 	RawRes() GoQLClientRunner
+	RetryAttempts(count int) GoQLClientRunner
+	RetryBackoff(backoff backoffFunc) GoQLClientRunner
+	RetryOn(fn func(err error) bool) GoQLClientRunner
+	RetryAllowStatus(fn func(int) bool) GoQLClientRunner
+}
+
+type GoQLClientResponse map[string]interface{}
+type GoQLClientError struct {
+	error
+	Description string
 }
 
 type Runner struct {
@@ -27,6 +36,15 @@ type Runner struct {
 	rawReq     bool
 	rawRes     bool
 	timeoutSec int
+	options    options
+	debug      bool
+}
+
+func (r *Runner) Debug() GoQLClientRunner {
+	r.debug = true
+	// r.RawReq()
+	// r.RawRes()
+	return r
 }
 
 func (r *Runner) RawReq() GoQLClientRunner {
@@ -39,36 +57,61 @@ func (r *Runner) RawRes() GoQLClientRunner {
 	return r
 }
 
-func (r *Runner) Run(ctx context.Context) interface{} {
-	ctx, cancel := context.WithTimeout(ctx, time.Second*time.Duration(r.timeoutSec))
-	defer cancel()
+func (r *Runner) RetryAttempts(count int) GoQLClientRunner {
+	r.options.Attempts = count
+	return r
+}
+
+func (r *Runner) RetryBackoff(backoff backoffFunc) GoQLClientRunner {
+	r.options.Backoff = backoff
+	return r
+}
+
+func (r *Runner) RetryOn(fn func(err error) bool) GoQLClientRunner {
+	r.options.RetryOn = fn
+	return r
+}
+
+func (r *Runner) RetryAllowStatus(fn func(int) bool) GoQLClientRunner {
+	r.options.AllowRetryStatus = fn
+	return r
+}
+
+func (r *Runner) Run(ctx context.Context) (GoQLClientResponse, *GoQLClientError) {
+
 	req, err := http.NewRequest(r.method, r.url, bytes.NewBuffer(r.body))
 	if err != nil {
-		L.Panic("ERROR from NewRequest", err)
+		L.Println("ERROR from NewRequest", err)
+		return nil, &GoQLClientError{
+			error:       err,
+			Description: "ERROR from NewRequest",
+		}
 	}
 	for h, hv := range r.headers {
 		req.Header.Set(h, hv)
 	}
 
-	req = req.WithContext(ctx)
-
 	if r.rawReq {
 		r.printRawRequest(req)
 	}
 
-	/*
-		resp, err := http.DefaultClient.Do(req)
-	*/
+	//req = req.WithContext(ctx)
+
 	resp, err := doWithRetry(ctx, req,
-		retryAttempts(5),
-		retryBackoff(linearBackoff(time.Second)),
-		retryOnFunc(func(err error) bool {
-			return strings.Contains(err.Error(), "timeout")
-		}),
-		allowRetryStatusFunc(func(i int) bool { return i >= 400 && i < 500 }),
+		time.Duration(r.timeoutSec),
+		retryAttempts(r.options.Attempts),
+		retryBackoff(r.options.Backoff),
+		retryOnFunc(r.options.RetryOn),
+		allowRetryStatusFunc(r.options.AllowRetryStatus),
+		enableDebugLog(&r.debug),
 	)
+
 	if err != nil {
-		L.Panic("ERROR from http client", err)
+		L.Println("ERROR from http client", err)
+		return nil, &GoQLClientError{
+			error:       err,
+			Description: "ERROR from http client",
+		}
 	}
 	defer resp.Body.Close()
 
@@ -78,16 +121,35 @@ func (r *Runner) Run(ctx context.Context) interface{} {
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		L.Panic("ERROR from ReadAll body", err)
+		L.Println("ERROR from ReadAll body", err)
+		return nil, &GoQLClientError{
+			error:       err,
+			Description: "ERROR from ReadAll body",
+		}
 	}
 
-	var response interface{}
-	err = json.Unmarshal(body, &response)
-	if err != nil {
-		L.Panic("ERROR from Unmarshal response", err)
+	// Check for GraphQL errors
+	var response map[string]interface{}
+	if err := json.Unmarshal(body, &response); err == nil {
+		if errors, ok := response["errors"]; ok {
+			for _, err := range errors.([]interface{}) {
+				errMsg := fmt.Sprintf("GraphQL error: message: %s\n", err.(map[string]interface{})["message"])
+				return nil, &GoQLClientError{
+					Description: errMsg,
+				}
+
+			}
+
+		}
+	} else {
+		L.Println("ERROR from Unmarshal response", err)
+		return nil, &GoQLClientError{
+			error:       err,
+			Description: "ERROR from Unmarshal response",
+		}
 	}
 
-	return response
+	return response, nil
 }
 
 func (r *Runner) printRawRequest(req *http.Request) {
@@ -96,7 +158,7 @@ func (r *Runner) printRawRequest(req *http.Request) {
 		L.Fatal(err)
 	}
 
-	L.Printf("----------------:REQUEST:----------------\n%s", string(reqDump))
+	L.Printf("\n----------------:REQUEST:----------------\n%s", string(reqDump))
 }
 
 func (r *Runner) printRawResponse(resp *http.Response) {
@@ -106,116 +168,4 @@ func (r *Runner) printRawResponse(resp *http.Response) {
 	}
 
 	L.Printf("\n----------------:RESPONSE:----------------\n%s", string(respDump))
-}
-
-// RetryAttempts configures the number of retry attempts
-func retryAttempts(attempts int) func(*retryOptions) {
-	return func(b *retryOptions) {
-		b.Attempts = attempts
-	}
-}
-
-// RetryBackoff configures the backoff duration between retries
-func retryBackoff(backoff backoffFunc) func(*retryOptions) {
-	return func(b *retryOptions) {
-		b.Backoff = backoff
-	}
-}
-
-// RetryOnFunc configures a function to decide retry based on error
-func retryOnFunc(fn func(error) bool) func(*retryOptions) {
-	return func(b *retryOptions) {
-		b.RetryOn = fn
-	}
-}
-
-// AllowRetryStatusFunc configures a function to allow retry based on status code
-func allowRetryStatusFunc(fn func(int) bool) func(*retryOptions) {
-	return func(b *retryOptions) {
-		b.AllowRetryStatus = fn
-	}
-}
-
-func doWithRetry(ctx context.Context, req *http.Request, opts ...retryOption) (*http.Response, error) {
-	// Apply options
-	options := defaultRetryOptions
-	for _, opt := range opts {
-		opt(&options)
-	}
-
-	client := &http.Client{}
-
-	for attempt := 0; attempt < options.Attempts; attempt++ {
-		select {
-		case <-ctx.Done():
-			return nil, ctx.Err()
-		default:
-		}
-
-		resp, err := client.Do(req)
-
-		if err == nil {
-			// Successful request
-			return resp, nil
-		}
-
-		if !options.RetryOn(err) {
-			// Error is not eligible for retry
-			return nil, err
-		}
-
-		if resp != nil && !options.AllowRetryStatus(resp.StatusCode) {
-			// Status code is not eligible for retry
-			return resp, err
-		}
-
-		// Backoff before retrying
-		options.Backoff(attempt)
-	}
-
-	err := fmt.Errorf("exceeded max attempts")
-	// Exceeded max attempts
-	return nil, err
-
-}
-
-// RetryOption defines a function to configure retry behavior
-type retryOption func(*retryOptions)
-
-// RetryOptions holds configurable retry parameters
-type retryOptions struct {
-	Attempts         int              `json:"attempts"`
-	Backoff          backoffFunc      `json:"backoff"`
-	RetryOn          func(error) bool `json:"retryOn"`
-	AllowRetryStatus func(int) bool   `json:"allowRetryStatus"`
-}
-
-var defaultRetryOptions = retryOptions{
-	Attempts:         3,
-	Backoff:          exponentialBackoff(time.Second),
-	RetryOn:          isTransientError,
-	AllowRetryStatus: func(i int) bool { return i >= 500 && i < 600 },
-}
-
-// IsTransientError checks if the error is transient
-func isTransientError(err error) bool {
-	// Implement your logic to identify transient errors based on error types
-	// or specific error messages
-	return false
-}
-
-// BackoffFunc determines the delay between retries.
-type backoffFunc func(attempt int)
-
-func exponentialBackoff(base time.Duration) backoffFunc {
-	return func(attempt int) {
-		duration := time.Duration(math.Pow(2, float64(attempt))) * base
-		time.Sleep(duration)
-	}
-}
-
-func linearBackoff(duration time.Duration) backoffFunc {
-	return func(attempt int) {
-		time.Sleep(duration + time.Duration(attempt))
-	}
 }
